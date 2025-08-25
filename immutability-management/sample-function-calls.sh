b4556468c04a236c8ff8c8279172bcf01599cb92
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2025-08-25 18:33:38 +0200

    Update sample-function-calls.sh
    Updating function calls examples

--- immutability-management/sample-function-calls.sh
+++ immutability-management/sample-function-calls.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
-az config set extension.use_dynamic_install=no --only-show-errors
-
 # Load config and variable definitions
 source "$(dirname "$0")/test-infra-config.sh"
 
 # Source the function definitions from files in the same folder
-source "$(dirname "$0")/get-rsvs-by-immutability-state.sh"
-source "$(dirname "$0")/set-immutability-for-vaults.sh"
+source "./get_rsvs_by_immutability_state.sh"
+source "./set_immutability_for_vaults.sh"
 
 # Sample calls to the functions
-#get_rsvs_by_immutability_state "$sub" "null,Disabled,Unlocked" "json"
-#get_rsvs_by_immutability_state "$sub" "Disabled,Unlocked" json
+sub="your-subscription-id"
+states=("null" "Disabled" "Unlocked" "Locked")
+get_rsvs_by_immutability_state $sub states json
 
 # Transition specified RSV states to desired state
-set_immutability_for_vaults "$(get_rsvs_by_immutability_state "$sub" "Locked" json)" "Disabled" "$sub"
+states=("Unlocked")
+set_immutability_for_vaults "$(get_rsvs_by_immutability_state "$sub" states json)" "Disabled" "$sub"
